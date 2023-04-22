777b9cc2213c1696bfb1c839c55e4ef358ece0bd
Keshav Kumar <keshav.kumar@practo.com>
2023-04-22 20:34:33 +0530

    amplify backend api setup with cloud function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyrestdemo": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "amplifydemoappb8018008": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
